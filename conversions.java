02da09969b7f08445ed9fafd92abb05f05296179
Topias-bvs <torousu01@bvsd.org>
2025-08-20 18:56:44 +0000

    added menu

--- conversions.java
+++ conversions.java
@@ -1,1 +1,15 @@
+// Topias Rousu         
+// 08/20/25
 
+public class conversions
+{
+    public static void main(string[] args);   
+    {
+        system.out.println("1. Fahrenheit to celsius");
+        system.out.println("2. Celcius to fahrenheit");
+        system.out.println("3. Feet to meters");
+        system.out.println("4. Meters to Feet");
+        system.out.println("");
+    }
+
+}
